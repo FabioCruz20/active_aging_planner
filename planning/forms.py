6acfea856725c5bbbe824f9d94d73dbeb6e10484
FabioCruz20 <fabiovict95@gmail.com>
2025-08-31 21:46:52 -0300

    refactor/remove-project - remove project model and its references

--- planning/forms.py
+++ planning/forms.py
@@ -1,21 +1,6 @@
 from django import forms
 from . import models
 
-class ProjectForm(forms.ModelForm):
-    '''
-    '''
-    class Meta:
-        model = models.Project
-        fields = ['name', 'description'] 
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'block w-full mb-4'
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'block w-full mb-4'
-            }),
-        }   
-
 
 class ActionForm(forms.ModelForm):
     '''
--- planning/urls/projects_urls.py
+++ planning/urls/projects_urls.py
@@ -4,14 +4,9 @@ from planning.views import projects_views
 app_name = 'projects'
 
 urlpatterns = [
-    path('', projects_views.list_view, name='list'),
-    path('new', projects_views.new_view, name='new'),
-    path('<int:project_id>/edit', projects_views.edit_view, name='edit'),
-    path('<int:project_id>/delete', projects_views.delete, name='delete'),
-    path('<int:project_id>/manage_levels', projects_views.manage_project_levels_view, name='manage_levels'),
-    path('<int:project_id>/manage_actions_matrix', projects_views.manage_actions_matrix_view, name='manage_actions_matrix'),
+    path('manage_actions_matrix', projects_views.manage_actions_matrix_view, name='manage_actions_matrix'),
     path('level/<int:projectlevel_id>/manage_actions', projects_views.manage_level_actions_view, name='manage_actions'),
-    path('<int:project_id>/manage-axis-level-actions/<int:axis_id>/<int:level_id>/', 
+    path('manage-axis-level-actions/<int:axis_id>/<int:level_id>/', 
          projects_views.manage_axis_level_actions_view, name='manage_axis_level_actions'),
     path('action/<int:project_action_id>/tasks/', 
          projects_views.manage_tasks_view, name='manage_tasks'),
