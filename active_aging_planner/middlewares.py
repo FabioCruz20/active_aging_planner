5e8c41542093ec45f11fd5e0abb47d66dfabeb7e
FabioCruz20 <fabiovict95@gmail.com>
2025-10-07 22:56:37 -0300

    feat: require login to views

---
+++ active_aging_planner/middlewares.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class LoginRequiredMiddleware:
+    """Middleware to require login for all views except login."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        path = request.path
+        allowed_urls = [
+            reverse('auth:login'),
+        ]
+
+        # Check if user is authenticated, if path is in allowed urls, or if path is related to admin pages or django live server.
+        if (
+            request.user.is_authenticated 
+            or path.startswith('/__reload__/')
+            or path.startswith('/admin/')
+            or path in allowed_urls
+        ):
+            return self.get_response(request)
+        
+        return redirect('auth:login')
--- active_aging_planner/settings.py
+++ active_aging_planner/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'active_aging_planner.middlewares.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'active_aging_planner.urls'
