86cff6a61754e3abf4968a9103e21ee236264493
FabioCruz20 <fabiovict95@gmail.com>
2025-10-20 22:20:28 -0300

    style: adjust paper pages styles

--- planning/forms.py
+++ planning/forms.py
@@ -61,10 +61,10 @@ class PaperForm(forms.ModelForm):
         fields = ['title', 'url']
         widgets = {
             'title': forms.TextInput(attrs={
-                'class': 'block w-full mb-4'
+                'class': 'block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
             }),
             'url': forms.URLInput(attrs={
-                'class': 'block w-full mb-4'
+                'class': 'block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
             }),
         }
 
